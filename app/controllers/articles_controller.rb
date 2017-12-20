51791d645e1f30758d98c38f4c19f372ce1889c0
sabreen.99 <sabreen.ahmed.222@gmail.com>
2017-12-20 12:19:46 +0200

    implementing delete article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
   def index
     @articles = Article.all
   end
@@ -18,13 +19,12 @@ def create
 end
 
 def show
-  @article = Article.find(params[:id])
 end
+
 def edit
-  @article = Article.find(params[:id])
 end
+
 def update
-  @article = Article.find(params[:id])
   if @article.update(article_params)
     flash[:success] = "Article has been updated"
     redirect_to article_path(@article)
@@ -33,6 +33,14 @@ def update
     render :edit
   end
 end
+
+def destroy
+  if @article.destroy
+    flash[:success] = "Article has been deleted"
+    redirect_to articles_path
+  end
+end
+
 protected
 
 def resource_not_found
@@ -42,6 +50,10 @@ def resource_not_found
 end
 
 private
+
+def set_article
+  @article = Article.find(params[:id])
+end
 
 def article_params
 	params.require(:article).permit(:title, :body)
